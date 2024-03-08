39e4c538dcc3f9022842e81135f9aa272453f208
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-03-08 12:11:17 +0100

    Aggiunto codice per funzione modulo in assembly

---
+++ modulo.asm
@@ -0,0 +1,30 @@
+.data
+	
+
+.text
+	# inizializzo i registri a 0
+	mul $t0, $t0, $zero
+	mul $t1, $t1, $zero
+	mul $t2, $t2, $zero
+	
+	# imposto il numeratore in $t0
+	addi $t0, $zero, 20
+	
+	# imposto il denominatore in $t1
+	addi $t1, $zero, 4  # $t1 denominatore
+	
+	Start:
+		# se numeratore < denominatore vai a End
+		blt $t0, $t1, End
+		# numeratore = numeratore - denominatore
+		sub $t0, $t0, $t1
+		# incremento il contatore
+		addi $t2, $t2, 1
+		# torno a Start
+		j Start
+	
+	End:
+		# stampo il resto
+		li $v0, 1
+		add $a0, $t0, $zero
+		syscall
